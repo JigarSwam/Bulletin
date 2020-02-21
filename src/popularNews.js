41a0ec3114f21dd7dc7df58c8181e3b6592a1635
Saad Salman <saaduhbucs@gmail.com>
2020-02-20 18:46:58 -0800

    created popularNews

--- src/popularNews.js
+++ src/popularNews.js
@@ -1,10 +1,4 @@
 import React from 'react';
-let api = 'http://newsapi.org/v2/';
-const API_KEY = '57e4c06b9dcf49ac93575f6a042c31bf';
-
-const apiHost = host => {
-    api = host;
-}
 export default function popularNews( {} ) {
 
 }
