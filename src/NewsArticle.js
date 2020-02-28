cd7fe610b054334c82ca0ee2009bded132ba04fa
Saad Salman <saaduhbucs@gmail.com>
2020-02-27 19:42:48 -0800

    Added functionality for API Call and Variable Articles

--- src/NewsArticle.js
+++ src/NewsArticle.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {newsArticles} from './mockAPI';
+import {popArticles} from './popNewsAPI';
 import Card from 'react-bootstrap/Card'
 
 export default function NewsArticle ({ article }) {
@@ -8,25 +9,19 @@ export default function NewsArticle ({ article }) {
     };
 
     return (
-        <Card style={{ width: '90rem' }} href={ article && article.url }>
+        <Card style={{ width: '50rem' }} href={ article && article.url }>
           <Card.Body>
-          <Card.Img variant="left" style={style} src={ article && article.urlToImage } />
+          <Card.Img variant="top" style={style} src={ article && article.urlToImage } />
             <Card.Title> 
               { article && article.title }
             </Card.Title>
             <Card.Subtitle>
-              { article && article.name }
+              { article && article.description }
             </Card.Subtitle>
             <Card.Text>
-              { article && article.description }
+              
             </Card.Text>
           </Card.Body>
         </Card>
-
-        // <Card>
-        // <img style = {style} src={ article && article.urlToImage } ></img>
-        // <a href={ article && article.url }>{ article && article.title }</a>
-        // <p>{ article && article.description }</p>
-        // </Card>
     )
 }
--- src/popularNews.js
+++ src/popularNews.js
@@ -1,25 +1,29 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {newsArticles} from './mockAPI';
 import NewsArticle from './NewsArticle';
 
-
 const PopularNews = ({ index }) => {
-    const [article, setArticle] = useState([]);
+    const [feed, setFeed] = useState([]);
 
-    const displayData = async event => {
-            const result = await newsArticles();
-            setArticle(result.articles);
-    }
-     
+    // useEffect(() => {
+    //     displayData();
+    // }, []);
 
+    useEffect(() => {
+        const API_KEY = `&apiKey=7c33092ea64d4e8990ae75778715703e`;
+        const URL = `http://newsapi.org/v2/top-headlines?country=us`;
+        let req = `${URL}${API_KEY}`;
+        fetch(req).then(r => r.json()).then(r => setFeed(r));
+      }, []);
+      
     return (
+        feed.articles ? (
         <div>
-            <button onClick={displayData}> Click to see news</button>
-            <NewsArticle article = {article[0]}/>
-            <NewsArticle article = {article[1]}/>
-            <NewsArticle article = {article[2]}/>
-            <NewsArticle article = {article[3]}/>
-        </div>
+            {feed.articles.map(article =>  <NewsArticle article= {article}></NewsArticle>)}
+            // <NewsArticle article = {feed.articles[0]}/>
+            // <NewsArticle article = {feed.articles[1]}/>
+            // <NewsArticle article = {feed.articles[2]}/>
+        </div>) : "No data yet"
     )
 }
 
