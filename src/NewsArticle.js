62d3e918dca3443c8f3be36b75918661fc35ce94
Saad Salman <saaduhbucs@gmail.com>
2020-02-27 18:48:27 -0800

    Articles moved to formatted cards

--- src/NewsArticle.js
+++ src/NewsArticle.js
@@ -8,27 +8,25 @@ export default function NewsArticle ({ article }) {
     };
 
     return (
-        // <Card>
-        //   <Card.Img>
-            
-        //   </Card.Img>
-        //   <Card.body>
-        //     <Card.Title> 
-        //       //Title of Article
-        //     </Card.Title>
-        //     <Card.Subtitle>
-        //       //Name of Publisher/Site
-        //     </Card.Subtitle>
-        //     <Card.text>
-        //       //Description/Summary of Article
-        //     </Card.text>
-        //   </Card.body>
-        // </Card>
-
-        <Card>
-        <img style = {style} src={ article && article.urlToImage } ></img>
-        <a href={ article && article.url }>{ article && article.title }</a>
-        <p>{ article && article.description }</p>
+        <Card style={{ width: '90rem' }} href={ article && article.url }>
+          <Card.Body>
+          <Card.Img variant="left" style={style} src={ article && article.urlToImage } />
+            <Card.Title> 
+              { article && article.title }
+            </Card.Title>
+            <Card.Subtitle>
+              { article && article.name }
+            </Card.Subtitle>
+            <Card.Text>
+              { article && article.description }
+            </Card.Text>
+          </Card.Body>
         </Card>
+
+        // <Card>
+        // <img style = {style} src={ article && article.urlToImage } ></img>
+        // <a href={ article && article.url }>{ article && article.title }</a>
+        // <p>{ article && article.description }</p>
+        // </Card>
     )
 }
