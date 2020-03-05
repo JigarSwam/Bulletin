84d2f04074cdbe7342154b3293f8f4e73f54916e
Saad Salman <saaduhbucs@gmail.com>
2020-03-05 14:48:54 -0800

    Added Tabs for each component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,28 @@ import './App.css';
 import PopularNews from "./PopularNews";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SearchBar from "./SearchBar"
+import Tabs from 'react-bootstrap/Tabs'
+import Tab from 'react-bootstrap/Tabs'
 
 
 function App() {
   return (
     <div className="App">
       <img src={process.env.PUBLIC_URL + "/bulletin_logo.png"} alt="bulletin logo!"  height="200" width="auto"/>
-      <SearchBar />
-
-      <h1> Popular News </h1>
-      <PopularNews index = {0}/>
+      <Tabs> 
+        <Tab eventKey="Popular" title="Popular News">
+        <h1> Popular News </h1>
+        <PopularNews index = {0}/>
+        </Tab>
+        <Tab eventKey="Search" title="Search">
+        <SearchBar />
+        </Tab>
+        <Tab eventKey="Sources" title="Sources">
+        
+        </Tab>
+      </Tabs>
+      
+      
     </div>
   );
 }
