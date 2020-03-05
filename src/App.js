1d8cd97fbf0bf890f8387eb146877befd609d6e2
Saad Salman <saaduhbucs@gmail.com>
2020-03-04 17:46:34 -0800

    Changed File Names

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import PopularNews from "./popularNews";
+import PopularNews from "./PopularNews";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SearchBar from "./SearchBar"
 
